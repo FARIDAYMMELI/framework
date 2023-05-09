361ff17315d81fd27ebb21555760178db4150049
ishakrahman007 <write2greenrahman@gmail.com>
2023-05-09 15:22:30 -0400

    show branching

--- src/test/java/orange/Feature1.java
+++ src/test/java/orange/Feature1.java
@@ -1,4 +1,95 @@
 package orange;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import team2.SetUp;
 
-public class Feature1 {
-}
+public class Feature1 extends SetUp {
+    Logger log = LogManager.getLogger(Feature1.class.getName());
+    @Test
+    public void validCred() throws InterruptedException {
+
+       // String expectedTitle = "OrangeHRM";
+       // String actualTitle = getCurrentTitle();
+       // Assert.assertEquals(expectedTitle,actualTitle);
+
+        //enter username, password, and click on login button
+        type("input[name='username']","Admin");
+        log.info("enter username success");
+        type("input[type='password']","admin123");
+        log.info("enter password success");
+        clickOn(".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button");
+        log.info("click on login button Success");
+
+        //check user is logged in
+        String expectedHomePageHeader = "Dashboard";
+        String actualHomePageHeader = getElementText(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module");
+        Assert.assertEquals(expectedHomePageHeader, actualHomePageHeader);
+        log.info("user logged in success");
+    }
+
+    @Test
+    public void invalidUsername(){
+        String expectedTitle = "OrangeHRM";
+        String actualTitle = getCurrentTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+        //enter  username, password, and click on login button
+        type("input[name='username']","Admingo");
+        log.info("enter username success");
+        type("input[type='password']","admin123");
+        log.info("enter password success");
+        clickOn(".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button");
+        log.info("click on login button Success");
+
+        //validate the error message
+        String expectedError = "Invalid credentials";
+        String actualError = getElementText(".oxd-text.oxd-text--p.oxd-alert-content-text");
+        Assert.assertEquals(expectedError, actualError);
+        log.info("validate error success");
+    }
+
+    @Test
+    public void missingUsername(){
+        String expectedTitle = "OrangeHRM";
+        String actualTitle = getCurrentTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+        //enter  username, password, and click on login button
+        type("input[name='username']","");
+        log.info("enter username success");
+        type("input[type='password']","admin123");
+        log.info("enter password success");
+        clickOn(".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button");
+        log.info("click on login button Success");
+
+        //validate the error message
+        String expectedError = "Required";
+        String actualError = getElementText(".oxd-text.oxd-text--span.oxd-input-field-error-message.oxd-input-group__message");
+        Assert.assertEquals(expectedError, actualError);
+        log.info("validate error success");
+    }
+    @Test
+    public void missingPassword(){
+        String expectedTitle = "OrangeHRM";
+        String actualTitle = getCurrentTitle();
+        Assert.assertEquals(expectedTitle, actualTitle);
+        //enter  username, password, and click on login button
+        type("input[name='username']","Admin");
+        log.info("enter username success");
+        type("input[type='password']","");
+        log.info("enter password success");
+        clickOn(".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button");
+        log.info("click on login button Success");
+
+        //validate the error message
+        String expectedError = "Required";
+        String actualError = getElementText(".oxd-text.oxd-text--span.oxd-input-field-error-message.oxd-input-group__message");
+        Assert.assertEquals(expectedError, actualError);
+        log.info("validate error success");
+    }
+
+
+    }
+
+
+
--- src/test/java/orange/Logout.java
+++ src/test/java/orange/Logout.java
@@ -1,51 +1,50 @@
-package opencart;
+package orange;
 
+import scaledupit.LogoutTest;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import team2.SetUp;
 
-public class LogoutTest extends SetUp {
+public class Logout extends SetUp {
+
     Logger log = LogManager.getLogger(LogoutTest.class.getName());
 
     @Test
     public void logout() throws InterruptedException {
-        String expectedTitle = "Swag Labs";
+        String expectedTitle = "OrangeHRM";
         String actualTitle = getCurrentTitle();
         Assert.assertEquals(expectedTitle, actualTitle);
         //enter  username, password, and click on login button
-        type("#user-name","standard_user");
+        type("input[name='username']","Admin");
         log.info("enter username success");
-        type("#password","secret_sauce");
+        type("input[type='password']","admin123");
         log.info("enter password success");
-        clickOn("#login-button");
+        clickOn(".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button");
         log.info("click on login button Success");
 
         //check user is logged in
-        String expectedHomePageHeader = "Products";
-        String actualHomePageHeader = getElementText("//span[contains(text(),'Products')]");
+        String expectedHomePageHeader = "Dashboard";
+        String actualHomePageHeader = getElementText(".oxd-text.oxd-text--h6.oxd-topbar-header-breadcrumb-module");
         Assert.assertEquals(expectedHomePageHeader, actualHomePageHeader);
         log.info("user logged in success");
 
-        //click on hamburger menu
-        clickOn("#react-burger-menu-btn");
-        log.info("click on hamburger menu success");
+        //click on dropdown
+        clickOn(".oxd-icon.bi-caret-down-fill.oxd-userdropdown-icon");
+        log.info("click on dropdown success");
         Thread.sleep(1000);
 
-        //hover hover & click on logout link
-        hoverOver("#logout_sidebar_link");
+        clickOn("//a[text()=\"Logout\"]");
         log.info("click on logout link success");
 
         //check user is landed to the login page
-        boolean loginPageHeaderIsDisplayed = isVisible("//div[contains(text(),'Swag Labs')]");
+        boolean loginPageHeaderIsDisplayed = isVisible(".oxd-button.oxd-button--medium.oxd-button--main.orangehrm-login-button");
         Assert.assertTrue(loginPageHeaderIsDisplayed);
         log.info("login page header is displayed");
-        String expectedLoginPageHeaderText = "Swag Labs";
-        String actualLoginPageHeaderText = getElementText("//div[contains(text(),'Swag Labs')]");
+        String expectedLoginPageHeaderText = "Login";
+        String actualLoginPageHeaderText = getElementText(".oxd-text.oxd-text--h5.orangehrm-login-title");
         Assert.assertEquals(expectedLoginPageHeaderText, actualLoginPageHeaderText);
         log.info("login page header text validation success");
-    }
-
 }
-
+}
